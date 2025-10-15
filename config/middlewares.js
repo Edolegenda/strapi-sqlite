0a18a1f8fe9e87c7cc928b490d7c391e3c1991ca
Edolegenda <159014001+Edolegenda@users.noreply.github.com>
2025-10-15 23:44:46 +0200

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,22 @@
 module.exports = [
-  "strapi::errors",
-  "strapi::security",
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::logger",
-  "strapi::query",
-  "strapi::body",
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'http://localhost:3000',     // Für lokale Tests
+        'https://dealcheck.ch',      // Deine Domain
+        'https://dealcheck.vercel.app', // Falls du Vercel-Preview nutzt
+      ],
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
   'strapi::session',
-  "strapi::favicon",
-  "strapi::public",
+  'strapi::favicon',
+  'strapi::public',
 ];
